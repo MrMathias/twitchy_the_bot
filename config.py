713779ffd0dade0628d6fad5b9df26757956555b
Mathias Andersen <MrMathiasMa@gmail.com>
2014-07-29 22:30:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-username = "username"
-password = "password"
-subreddit = "subreddit"
-wiki_stream_config = "streamconfig"
-wiki_streams = "streams"
+username = "bot_name"
+password = "bot_pass"
+subreddit = "Dota2"
+twitchgame = "Dota 2"
+streamnum = 3# 0 to 8, limited by the image size on reddit. If you want more, you need to lower the quality or size of the thumbnails
